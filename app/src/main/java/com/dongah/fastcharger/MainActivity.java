020ba7fab92768665fc6e67c9c4de38c5a574c50
ghchoi@dongahelecomm.co.kr <ghchoi@dongahelecomm.co.kr>
2026-09-02 14:42:38 +0900

    onActivityResult 콜백 오버라이드해서 외부 결제 결과 처리

--- app/src/main/java/com/dongah/fastcharger/MainActivity.java
+++ app/src/main/java/com/dongah/fastcharger/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -383,6 +384,15 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode  == VCatPaymentManager.getInstance().getAwakeRequestCode()) {
+            VCatPaymentManager.getInstance().onVCatAwakeResult();
+        }
+    }
+
     private void hideNavigationBar() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
@@ -574,6 +584,7 @@ public class MainActivity extends AppCompatActivity {
         if (monitorHttpServer != null) {
             monitorHttpServer.stopServer();
         }
+        VCatPaymentManager.getInstance().unbind();
 //        if (serviceProcessingActivity != null) serviceProcessingActivity.unbind();
         super.onDestroy();
     }
